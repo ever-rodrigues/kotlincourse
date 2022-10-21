d1bf6d6f5a7f9a2223c5ada1d8065a74d3776a8c
ever-rodrigues <evertonrodriguespuc@gmail.com>
2022-10-21 19:56:49 -0300

    update with controlflux,generics,loop

--- KotlinCourse-Demo/src/main/kotlin/com/course/LoopUse.kt
+++ KotlinCourse-Demo/src/main/kotlin/com/course/LoopUse.kt
@@ -6,8 +6,39 @@ class LoopUse {
 
 fun main() {
 
+    val myTeste= teste()
+
     val cakes= listOf("carrot","cheese","cholocalte")
     for(cake in cakes){
         println(cake)
     }
+
+    var cakeBaked =0
+    var cakeEaten=0
+while(cakeEaten<5){
+    eatCake()
+    cakeEaten++
 }
+    do{
+        cakeBaked++
+    }while (cakeEaten>cakeBaked)
+}
+
+fun eatCake(){
+    println("Eating a cake")
+}
+
+
+fun teste(){
+    var teste=0
+    do{
+        teste++
+        println("teste")
+    }
+    while(teste<5)
+}
+
+
+//Iterators
+
+
